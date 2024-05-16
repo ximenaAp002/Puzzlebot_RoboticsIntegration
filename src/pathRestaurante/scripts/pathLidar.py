35f35209283caf8da116e58eda31c85545ba4b3b
Monserrat Galván <55207425+mxgalvxn@users.noreply.github.com>
2024-05-15 20:26:17 -0600

    Update pathLidar.py

--- src/pathRestaurante/scripts/pathLidar.py
+++ src/pathRestaurante/scripts/pathLidar.py
@@ -6,9 +6,15 @@ import roslib; roslib.load_manifest('teleop_twist_keyboard')
 import rospy
 import math
 from geometry_msgs.msg import Twist, PoseStamped
-from std_msgs.msg import Int16, Bool
+from std_msgs.msg import Int16, Bool, Float32
+import random
 
-# Define callback functions for wheel velocities
+t = 0
+wmax = .2 
+dt = 0.01
+kpr = 1.4
+kpt = 3.0
+
 def posY_callback(data):
     global posY
     posY = data.pose.position.y
@@ -17,14 +23,32 @@ def posX_callback(data):
     global posX
     posX = data.pose.position.x
 
-def posZ_callback(data):
-    global posZ
-    posZ = data.pose.orientation.w
+def ori_callback(data):
+    global ori
+    ori = data.data
 
 def lidar_callback(data):
     global stop
     stop = data.data
-# Set up ROS node and publishers/subscribers
+
+def ruta():
+    numPath = rospy.get_param("/ruta/numMesa")
+    numPath = 1
+    ruta_archivo = "/home/karma/gazebo/src/pathRestaurante/paths/path" + str(numPath) + "Try1.txt"
+
+    datos = np.loadtxt(ruta_archivo, skiprows=1)
+
+    posiciones_x = .01 * datos[:, 0]
+    posiciones_y = .01 * datos[:, 1]
+
+    diferenciaX = posiciones_x[0]
+    diferenciaY = posiciones_y[0]
+
+    for i in range(len(posiciones_x)):
+        posiciones_x[i] -= diferenciaX
+        posiciones_y[i] -= diferenciaY
+    
+    return  posiciones_x, posiciones_y
 
 if __name__=="__main__":
     try:
@@ -34,63 +58,36 @@ if __name__=="__main__":
         rate = rospy.Rate(nodeRate)
 
         twist = Twist()
-        # if stop:
-        #     vmax = 0
 
-        # else: 
-        vmax = 0.05
-        t = 0  # Total time (s)
-        wmax = .2 # maximum angular velocity (rad/s)
-        dt = 0.01
-        # Read positions from file
-        numPath = rospy.get_param('ruta/numMesa')
-        numPath = 1
-        ruta_archivo = "/home/karma/gazebo/src/pathRestaurante/paths/path" + str(numPath) + "Try1.txt"
-
-        datos = np.loadtxt(ruta_archivo, skiprows=1)
-
-        posiciones_x = .01 * datos[:, 0]
-        posiciones_y = .01 * datos[:, 1]
-
-        diferenciaX = posiciones_x[0]
-        diferenciaY = posiciones_y[0]
-
-        for i in range(len(posiciones_x)):
-            posiciones_x[i] -= diferenciaX
-            posiciones_y[i] -= diferenciaY
+        posiciones_x, posiciones_y = ruta()
 
         posiciones_x_rev = np.flip(posiciones_x)
         posiciones_y_rev = np.flip(posiciones_y)
 
-        #print("Posiciones de x:", posiciones_x)
-        #print("Posiciones de y:", posiciones_y)
-
-        #print("Posiciones de x:", posiciones_x_rev)
-        #print("Posiciones de y:", posiciones_y_rev)
-
         num_positions = len(posiciones_x)
 
-        #pos_x = positions[]
-        #print(positions[0])
-        #rospy.sleep(1000)
-        kpr = 1.4
-        kpt = 3.0
         error_dis_ant = 0.0
         error_ori_ant = 0.0
 
-
         posY = 0.0
         posX = 0.0
-        posZ = 0.0
+        ori = 0.0
 
-        # Simulate robot motion
+        stop = False
+
         for k in range (2):
             pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
 
-            orientacion = rospy.Subscriber('/slam_out_pose', PoseStamped, posZ_callback)
+            orientacion = rospy.Subscriber('/z_Euler', Float32, ori_callback)
             pos_act_x = rospy.Subscriber('/slam_out_pose', PoseStamped, posX_callback)
             pos_act_y = rospy.Subscriber('/slam_out_pose', PoseStamped, posY_callback)
+
             rospy.Subscriber("/stop", Bool, lidar_callback)
+            if stop:
+                vmax = 0
+            else: 
+                vmax = 0.05
+
             i = 0
             reverse = False
             if k == 1:
@@ -106,17 +103,14 @@ if __name__=="__main__":
                 print("x: ", posX, "x deseada: ", x_des)
                 print("y: ", posY, "y deseada: ", y_des)
 
-                posZ = 
-
                 error_dis = math.sqrt((x_des-posX)**2 + (y_des-posY)**2)
-                error_ori = math.atan2(y_des-posY, x_des-posY) - posZ
+                error_ori = math.atan2(y_des-posY, x_des-posY) - ori
                 
                 if error_ori > math.pi:
                     error_ori = error_ori - 2*math.pi
                 elif error_ori < -math.pi:
                     error_ori = error_ori + 2*math.pi
 
-                
                 error_dis_der= error_dis - error_dis_ant
                 error_ori_der = error_ori - error_ori_ant
 
@@ -125,16 +119,6 @@ if __name__=="__main__":
                 
                 error_dis_ant = error_dis
                 error_ori_ant = error_ori
-                
-                #print('y actual = ', y, 'y deseada = ', yd)
-                #print(numPath)
-                #print('y deseada = ', y_des)
-                #print('x deseada = ', x_des)
-                # print()
-                #print('x actual = ', x_act, 'x deseada = ', xd)
-                # print()
-                # print('error =', error)
-                # print()
 
                 vr = u_vel+u_ori
                 vl = u_vel-u_ori
@@ -142,12 +126,10 @@ if __name__=="__main__":
                 vref = vmax * (vr + vl)/2
                 wref = wmax * (-kpr * error_ori)
 
-                # Compute wheel velocities from desired linear and angular velocities
                 twist.linear.x = vref
                 twist.angular.z = wref
                 pub.publish(twist)
 
-                # Check if the robot has reached the desired position
                 if abs(error_dis) < 0.01:
                     i += 1
                 t = t + dt
@@ -156,9 +138,10 @@ if __name__=="__main__":
             twist.linear.x = 0
             twist.angular.z = 0
             pub.publish(twist)
+            #if (pedido):
+                #ACTIVAR NODO
+                #HACER LOS PUBLISHERS
             rospy.sleep(10)
         print("mesa ", numPath )
     except rospy.ROSInterruptException:
         pass
-
-
